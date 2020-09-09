f0f67ca90f8162cf2ab280778cb1df1f717409ab
davidedicroce <davide.di.croce@cern.ch>
2020-09-09 12:41:00 -0700

    Update NV hackathon

--- multigpu_boostedjet_trainer.py
+++ multigpu_boostedjet_trainer.py
@@ -56,18 +56,6 @@ else:
     verbose = 0
 
 
-# After N batches, will output the loss and accuracy of the last batch tested
-#class NBatchLogger(keras.callbacks.Callback):
-#    def __init__(self, display=100):
-#        self.seen = 0
-#        # Display: number of batches to wait before outputting loss
-#        self.display = display
-#
-#    def on_batch_end(self, batch, logs={}):
-#        self.seen += logs.get('size',0)
-#        if self.seen % self.display == 0:
-#            print('\n{}/{} - Batch Accuracy: {},  Batch Loss: {}\n'.format(self.seen, self.params['nb_sample'], self.params['metrics'][0], logs.get('loss')))
-
 def LR_Decay(epoch):
     drop = 0.5
     epochs_drop = 10
@@ -83,9 +71,6 @@ def restart_epoch(args):
 
     return epoch
 
-#train_sz = 384000*2 #qg amount in paper
-#valid_sz = 12950 # qg amount in paper
-#test_sz = 69653 #qg amount in paper
 '''
 BATCH_SZ = 32
 train_sz = 32*81250
@@ -193,13 +178,10 @@ def create_resnet():
         print('Loading weights from file:', model_name)
         resnet.load_weights(model_name)
     #opt = keras.optimizers.Adam(lr=lr_init, epsilon=1.e-5) # changed eps to match pytorch value
-    ##Scale the learning rate by the number of workers
     #opt = keras.optimizers.SGD(lr=lr_init * hvd.size())
-    ##use the NovoGrad optimizer instead of SGD
     opt = NovoGrad(learning_rate=lr_init * hvd.size())
-    #Wrap the optimizer in a Horovod distributed optimizer
+    #Wrap the optimizer in a Horovod distributed optimizer -> uses hvd.DistributedOptimizer() to compute gradients.
     opt = hvd.DistributedOptimizer(opt)
-    # uses hvd.DistributedOptimizer() to compute gradients.
 
     #For Horovod: We specify `experimental_run_tf_function=False` to ensure TensorFlow
     resnet.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'], experimental_run_tf_function = False)
@@ -220,38 +202,15 @@ if __name__ == '__main__':
         if not os.path.isdir('%s/%s'%(d, expt_name)):
             os.makedirs('%s/%s'%(d, expt_name))
     
-    ## Create a MirroredStrategy.
-    #strategy = tf.distribute.MirroredStrategy()
-    #print("Number of devices: {}".format(strategy.num_replicas_in_sync))
-    ## Open a strategy scope.
-    #with strategy.scope():
-    #    # Everything that creates variables should be under the strategy scope. In general this is only model construction & `compile()`.
-    # 
     # Build network
     resnet = create_resnet()
 
-    #with tf.Session() as sess, graph.as_default():
-    #with graph.as_default():
-    #hooks = [hvd.BroadcastGlobalVariablesHook(0)]
-    #with tf.train.MonitoredTrainingSession(hooks=hook) as sess, graph.as_default():
-    
     # Model Callbacks
-    #print_step = 1000
-    #checkpoint = keras.callbacks.ModelCheckpoint('/uscms/home/ddicroce/work/QuarkGluon/CMSSW_9_4_17/src/QCD_Glu_Quark/MODELS/' + expt_name + '/epoch{epoch:02d}-{val_loss:.2f}.hdf5', verbose=1, save_best_only=False)#, save_weights_only=True)
-    #batch_logger = NBatchLogger(display=print_step)
-    #csv_logger = keras.callbacks.CSVLogger('%s.log'%(expt_name), separator=',', append=False)
-    #lr_scheduler = keras.callbacks.LearningRateScheduler(LR_Decay)
-    #callbacks_list=[checkpoint, csv_logger, lr_scheduler]
     callbacks_list = []
-    #callback_list.append(NBatchLogger(display=print_step))
     callbacks_list.append(myCallback())
-    ##implement a LR warmup over `args.warmup_epochs`
     callbacks_list.append(hvd.callbacks.LearningRateWarmupCallback(warmup_epochs=args.warmup_epochs, verbose=verbose))
-    ##replace with the Horovod learning rate scheduler, taking care not to start until after warmup is complete
     callbacks_list.append(hvd.callbacks.LearningRateScheduleCallback(start_epoch=args.warmup_epochs, multiplier=LR_Decay))
-    ##broadcast initial variable states from the first worker to all others by adding the broadcast global variables callback.
     callbacks_list.append(hvd.callbacks.BroadcastGlobalVariablesCallback(0))
-    ##average the metrics among workers at the end of every epoch by adding the metric average callback.
     callbacks_list.append(hvd.callbacks.MetricAverageCallback())  
     resume_from_epoch = 0
     #checkpointing should only be done on the root worker.
@@ -276,9 +235,6 @@ if __name__ == '__main__':
 
     history = resnet.fit(
         train_data,
-        #steps_per_epoch=train_sz//BATCH_SZ,
-        ##keep the total number of steps the same despite of an increased number of workers
-        #steps_per_epoch= train_sz // hvd.size(), 
         steps_per_epoch= train_sz // (BATCH_SZ*hvd.size()), 
         epochs=epochs,
         callbacks=callbacks_list,
@@ -286,10 +242,7 @@ if __name__ == '__main__':
         workers=hvd.size(),
         initial_epoch=resume_from_epoch,
         validation_data=val_data,
-        #validation_steps=valid_steps,
-        #set this value to be 3 * num_test_iterations / number_of_workers
-        #validation_steps= 3 * valid_sz // hvd.size())
-        validation_steps = valid_steps)
+        validation_steps = 3 * valid_steps)
         #initial_epoch = args.load_epoch)
     
     #y_iter = test_data[1].make_one_shot_iterator().get_next()
